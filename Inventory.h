6a32194c2829d1116ce6ecabcacde3101322fe2b
will_colding <william.colding@gmail.com>
2019-11-06 14:47:59 -0500

    Third time's the charm

--- Inventory.h
+++ Inventory.h
@@ -92,7 +92,7 @@ struct WWInventory
 		Songs		= Songs | patch.Songs;
 		Triforce	= Triforce | patch.Triforce;
 		Pearls	= Pearls | patch.Pearls;
-		patch.Charts.SetState(Charts.GetState() | patch.Charts.GetState());
+		Charts.SetState(Charts.GetState() | patch.Charts.GetState());
 
 		Hearts = patch.Hearts;
 		PiecesofHeart = patch.PiecesofHeart;
